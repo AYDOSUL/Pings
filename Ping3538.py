bb2560afd2884b51806e9c2221088b3964c34858
AYDOSUL <sural.adyoth@gmail.com>
2025-02-27 23:11:31 -0500

    Cleaning up some of the code

--- Ping3538.py
+++ Ping3538.py
@@ -4,26 +4,37 @@ import logging
 
 ip_list = ['10.35.38.2', '10.35.38.1', '10.35.38.10', '10.35.38.11', '10.35.38.4']
 i = 0
+
 logging.basicConfig(format="{asctime} - {levelname} - {message}", style="{", datefmt="%Y-%m-%d %H:%M",)
+
 print("Started logging:")
+
 name_list = ['RoboRio', 'Radio', 'Upper Coprocessor', 'Lower Coprocessor', 'FMS']
 devip_dict = dict(zip(ip_list, name_list))
+
 for ip in ip_list:
+
     response = os.popen(f"ping -n 1 -w 50 {ip}").read()
     if "Received = 1" in response:
         i = i + 1
         print(f"UP {ip} Ping Successful, {devip_dict[ip]} is UP!")
+
     elif "Request timed out." in response:
         logging.warning(f"TIMED OUT {ip} {devip_dict[ip]} Response was Timed Out, Retrying")
         response = os.popen(f"ping -n 1 {ip}").read()
+
         if "Received = 1" in response:
             i = i + 1
             print(f"UP {ip} Ping Successful, {devip_dict[ip]} is UP!")
+
         elif "Request timed out." in response:
-            logging.error(f"TIMED OUT {ip}, {devip_dict[ip]} is DOWN!") #Warning for now just to make sure VSC doesn't kill the program
+            logging.error(f"TIMED OUT {ip}, {devip_dict[ip]} is DOWN!")
+
 print("=======================================================================")
+
 if i == len(ip_list):
     print("All Hosts Active")
+
 else:
     miss = len(ip_list) - i
     length = len(ip_list)
