5b5c34d525b547668431dc5b9843fc7963bb77e7
AYDOSUL <sural.adyoth@gmail.com>
2025-02-27 22:49:37 -0500

    Added IP adresses and coprocessor names

--- Ping3538.py
+++ Ping3538.py
@@ -2,24 +2,25 @@ import os
 import time
 import logging
 
-ip_list = ['192.168.0.1', '8.8.4.4', '4.4.4.4', '1.1.1.1', '1.2.3.4']
+ip_list = ['10.35.38.2', '10.35.38.1', '10.35.38.10', '10.35.38.11', '10.35.38.4']
 i = 0
 logging.basicConfig(format="{asctime} - {levelname} - {message}", style="{", datefmt="%Y-%m-%d %H:%M",)
 print("Started logging:")
-
+name_list = ['RoboRio', 'Radio', 'Upper Coprocessor', 'Lower Coprocessor', 'FMS']
+devip_dict = dict(zip(ip_list, name_list))
 for ip in ip_list:
     response = os.popen(f"ping -n 1 -w 50 {ip}").read()
     if "Received = 1" in response:
         i = i + 1
-        print(f"UP {ip} Ping Successful, Host is UP!")
+        print(f"UP {ip} Ping Successful, {devip_dict[ip]} is UP!")
     elif "Request timed out." in response:
-        print(f"TIMED OUT {ip} Host Response was Timed Out, Retrying")
+        logging.warning(f"TIMED OUT {ip} {devip_dict[ip]} Response was Timed Out, Retrying")
         response = os.popen(f"ping -n 1 {ip}").read()
         if "Received = 1" in response:
             i = i + 1
-            print(f"UP {ip} Ping Successful, Host is UP!")
+            print(f"UP {ip} Ping Successful, {devip_dict[ip]} is UP!")
         elif "Request timed out." in response:
-            logging.error(f"TIMED OUT {ip} Host is DOWN!")
+            logging.error(f"TIMED OUT {ip}, {devip_dict[ip]} is DOWN!") #Warning for now just to make sure VSC doesn't kill the program
 print("=======================================================================")
 if i == len(ip_list):
     print("All Hosts Active")
